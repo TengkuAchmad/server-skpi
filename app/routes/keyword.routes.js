af2156f9c4b3ebfd84e57fe0aaa1a539dbc158bd
= <=>
2024-04-18 15:35:04 +0700

    Add Keyword Functionality

--- app/routes/keyword.routes.js
+++ app/routes/keyword.routes.js
@@ -7,12 +7,12 @@ const { authenticateToken } = require("../middleware/middleware");
 // ROUTER
 const router =  require("express").Router();
 
-router.post("keyword-management", authenticateToken, keyword.create);
-router.get("keyword-management/:id", authenticateToken, keyword.findOne);
-router.get("keyword-management", authenticateToken, keyword.findAll);
-router.put("keyword-management/:id", authenticateToken, keyword.update);
-router.delete("keyword-management", authenticateToken, keyword.deleteAll);
-router.delete("keyword-management/:id", authenticateToken, keyword.deleteOne);
+router.post("/keyword-management", authenticateToken, keyword.create);
+router.get("/keyword-management/:id", authenticateToken, keyword.findOne);
+router.get("/keyword-management", authenticateToken, keyword.findAll);
+router.put("/keyword-management/:id", authenticateToken, keyword.update);
+router.delete("/keyword-management", authenticateToken, keyword.deleteAll);
+router.delete("/keyword-management/:id", authenticateToken, keyword.deleteOne);
 
 
 module.exports = router
